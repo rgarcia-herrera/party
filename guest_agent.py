54e6588a14fa577df941be1fd4b5ca10c5c42309
rgarcia-herrera <rgarcia@inmegen.gob.mx>
2017-02-25 15:42:21 -0600

    ask for number of iterations

--- guest_agent.py
+++ guest_agent.py
@@ -4,7 +4,7 @@ from time import sleep
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from model import *
+import model
 
 ################################
 # parse command line arguments #
@@ -13,6 +13,7 @@ parser = argparse.ArgumentParser(description='edge interaction script')
 parser.add_argument('--db_url', default='sqlite:///party_sim.sqlite', help='DB URL, default: sqlite:///db.sqlite')
 parser.add_argument('--guest', type=int, required=True)
 parser.add_argument('--sleep', type=float, default=0.1)
+parser.add_argument('--iterations', type=int, default=200)
 args = parser.parse_args()
 
 
@@ -24,8 +25,8 @@ Session = sessionmaker(bind=engine)
 
 session = Session()
 
-g = Guest.query.get(args.guest)
-while True:
+g = model.Guest.query.get(args.guest)
+
+for i in range(args.iterations):
     g.update_happiness()
-    session.commit()
     sleep(args.sleep)
