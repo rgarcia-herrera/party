590d83e0376b88ecb0c824948e49f2fe05eb38fa
rgarcia-herrera <rgarcia@inmegen.gob.mx>
2017-02-26 16:59:00 -0600

    subplots share time/x

--- sim_plot.py
+++ sim_plot.py
@@ -5,7 +5,8 @@ import networkx as nx
 
 
 parser = argparse.ArgumentParser(description='plot simulation from log')
-parser.add_argument('log', type=argparse.FileType('r'), help='log file')
+parser.add_argument('--log', type=argparse.FileType('r'), help='path to log file')
+parser.add_argument('--fig', type=argparse.FileType('w'), help='path to write figure')
 args = parser.parse_args()
 
 csvr = csv.reader(args.log)
@@ -18,12 +19,13 @@ for row in csvr:
     h.append(row[1])
     b.append(row[2])
 
-# scatter plots
 plt.plot(t, h, 'r', t, b, 'b')
-plt.show()
-#plt.title("node levels")
-#plt.ylabel("level")
-#plt.xlabel("seconds")
 
-#plt.axes([0.45,0.45,0.45,0.45])
-#plt.axis('off')
+# Two subplots, the axes array is 1-d
+f, axarr = plt.subplots(2, sharex=True)
+axarr[0].plot(t, h, 'r')
+axarr[0].set_title('Happy party goers')
+axarr[1].plot(t, b, 'b')
+axarr[1].set_title('Same sex groups')
+
+plt.savefig(args.fig)
