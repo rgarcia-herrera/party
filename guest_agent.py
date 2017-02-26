995707219ce8509e80012f22d6d5830bf410a47d
rgarcia-herrera <rgarcia@inmegen.gob.mx>
2017-02-26 17:07:33 -0600

    use seconds instead of iterations

--- guest_agent.py
+++ guest_agent.py
@@ -1,6 +1,6 @@
 import argparse
 from time import sleep
-
+import datetime
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -13,7 +13,7 @@ parser = argparse.ArgumentParser(description='edge interaction script')
 parser.add_argument('--db_url', default='sqlite:///party_sim.sqlite', help='DB URL, default: sqlite:///db.sqlite')
 parser.add_argument('--guest', type=int, required=True)
 parser.add_argument('--sleep', type=float, default=0.1)
-parser.add_argument('--iterations', type=int, default=200)
+parser.add_argument('--seconds', type=int, default=200)
 args = parser.parse_args()
 
 
@@ -28,7 +28,13 @@ model.session=session
 
 g = session.query(model.Guest).get(args.guest)
 
-for i in range(args.iterations):
+now = datetime.datetime.now()
+current_sec = (now.minute*60000000)+(now.second*1000000)
+end_sec = current_sec + (args.seconds * 1000000)
+while current_sec < end_sec:
     g.update_happiness()
     g.leave_if_unhappy()
     sleep(args.sleep)
+    now = datetime.datetime.now()
+    current_sec = (now.minute*60000000)+(now.second*1000000)
+    
