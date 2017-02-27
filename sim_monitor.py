dbf37112e3287c522c1d2071c7d8fce7e6e3c9dc
rgarcia-herrera <rgarcia@inmegen.gob.mx>
2017-02-26 18:21:37 -0600

    monitor should also use a timer

--- sim_monitor.py
+++ sim_monitor.py
@@ -14,6 +14,7 @@ parser = argparse.ArgumentParser(description='Realtime monitor of simulation sta
 parser.add_argument('--db_url', default='sqlite:///party_sim.sqlite', help='DB URL, default: sqlite:///party_sym.sqlite')
 parser.add_argument('--sleep', type=float, default=0.1)
 parser.add_argument('--log', type=argparse.FileType('w'), default=sys.stdout, help='log to file')
+parser.add_argument('--seconds', type=int, default=11)
 args = parser.parse_args()
 
 ####################
@@ -33,13 +34,24 @@ happy_guests = session.query(model.Guest).filter(model.Guest.happy == True).coun
 total_guests = session.query(model.Guest).count()
 
 now = datetime.datetime.now()
-start_sec = (now.minute*60000000)+(now.second*1000000)+now.microsecond
+start_sec = (now.minute*60000000)+(now.second*1000000)
+current_sec = (now.minute*60000000)+(now.second*1000000)
+end_sec = current_sec + (args.seconds * 1000000)
 
-while happy_guests < total_guests:
+while (current_sec < end_sec):
+    sleep(args.sleep)
+    
     happy_guests = session.query(model.Guest).filter(model.Guest.happy == True).count()
     boring_groups = [g.boring() for g in session.query(model.Group).all()].count(True)
     now = datetime.datetime.now()
-    second = str((now.minute * 60000000) + (now.second * 1000000) + now.microsecond - start_sec)
+    second = str(((now.minute * 60000000) + (now.second * 1000000) - start_sec) + now.microsecond)
     log.writerow([second, happy_guests, boring_groups])
-    sleep(args.sleep)
+    
     session.commit()
+
+    if happy_guests == total_guests:
+        break
+
+    now = datetime.datetime.now()    
+    current_sec = (now.minute*60000000)+(now.second*1000000)
+
